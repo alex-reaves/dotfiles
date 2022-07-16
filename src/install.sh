d898db5c1bf0fc822f44895387202e2e5678c5fb
alex-reaves <alexreaves@protonmail.com>
2022-07-16 16:37:55 -0400

    Worked on install.sh

--- src/install.sh
+++ src/install.sh
@@ -18,6 +18,13 @@ sudo apt install -y gnome-session micro gnome-shell-extensions
 curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
 
 # adw-gtk3
-ADWGTKVERDOT=$(curl -s https://api.github.com/repos/lassekongo83/adw-gtk3/releases/latest | grep "tag_name" | awk '{print substr($2,2,length($2)-3)}')
-ADWGTKVER="${ADWGTKVERDOT/"."/"-"}"
-wget https://github.com/lassekongo83/adw-gtk3/releases/download/"$ADWGTKVERDOT"/adw-gtk3"$ADWGTKVER".tar.xz
+ADWGTKVERDOT=$(curl -s https://api.github.com/repos/lassekongo83/adw-gtk3/releases/latest  \
+| grep "tag_name" \
+| awk '{print substr($2,2,length($2)-3)}') # EX: `"tag_name": "v3.3",` to `v3.3`
+ADWGTKVERDASH="${ADWGTKVERDOT/"."/"-"}" # The releases are titled differently than the downloads, so replace "." with "-"
+wget https://github.com/lassekongo83/adw-gtk3/releases/download/"$ADWGTKVERDOT"/adw-gtk3"$ADWGTKVERDASH".tar.xz
+
+tar -xf adw-gtk3"$ADWGTKVER".tar.xz
+mkdir -p .local/share/themes
+mv adw-gtk3 .local/share/themes
+mv adw-gtk3-dark .local/share/themes
